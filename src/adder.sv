4e596826b32131e523e596a96cb383f37fa5d175
Tung Nguyen <tunghnguyen.ac@proton.me>
2023-11-29 15:55:38 -0800

    modified adder to also subtract

--- src/adder.sv
+++ src/adder.sv
@@ -1,17 +1,18 @@
 module adder #(
     parameter WIDTH = 32
 ) (
-    input  wire [WIDTH-1:0] a,
-    input  wire [WIDTH-1:0] b,
-    output reg  [WIDTH-1:0] o
+    input wire [WIDTH-1:0] a,
+    input wire [WIDTH-1:0] b,
+    input wire sub,
+    output reg [WIDTH-1:0] o
 );
 
   wire c[WIDTH-1:0]  /*verilator split_var*/;
   genvar i;
   full_adder fa (
       .a (a[0]),
-      .b (b[0]),
-      .ci(1'b0),
+      .b (b[0] ^ sub),
+      .ci(sub),
       .o (o[0]),
       .co(c[0])
   );
@@ -20,7 +21,7 @@ module adder #(
     for (i = 1; i < WIDTH; i = i + 1) begin : no
       full_adder fa (
           .a (a[i]),
-          .b (b[i]),
+          .b (b[i] ^ sub),
           .ci(c[i-1]),
           .o (o[i]),
           .co(c[i])
--- testbench/tb_adder.sv
+++ testbench/tb_adder.sv
@@ -2,29 +2,31 @@ module tb_adder #(
     parameter TB_WIDTH = 32
 );
 
-  reg  [TB_WIDTH-1:0] a;
-  reg  [TB_WIDTH-1:0] b;
-
+  reg [TB_WIDTH-1:0] a;
+  reg [TB_WIDTH-1:0] b;
+  reg sub;
   wire [TB_WIDTH-1:0] o;
 
   adder #(
       .WIDTH(TB_WIDTH)
   ) dut (
-      .a(a),
-      .b(b),
-      .o(o)
+      .a  (a),
+      .b  (b),
+      .sub(sub),
+      .o  (o)
   );
 
   function void init();
-    a <= 0;
-    b <= 0;
+    a   <= 0;
+    b   <= 0;
+    sub <= 0;
   endfunction
 
   task test();
     repeat (19) begin
-      #(20)
-      a <= $random % 50;
-      b <= $random % 50;
+      #(20) a <= $random % 50;
+      b   <= $random % 50;
+      sub <= $random % 2;
     end
   endtask
 
