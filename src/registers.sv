6c8f0c5243bdd6fba772cca97821f6c2c67b9b18
Tung Nguyen <tunghnguyen.ac@proton.me>
2023-12-11 23:45:08 -0800

    added the 32 general-purpose registers

---
+++ src/registers.sv
@@ -0,0 +1,23 @@
+module registers (
+    input wire [4:0] r_reg0,
+    input wire [4:0] r_reg1,
+    input wire [4:0] w_reg,
+    input wire [31:0] w_dat,
+    input wire write,
+    input wire clk,
+    output reg [31:0] r_dat0,
+    output reg [31:0] r_dat1
+);
+
+  reg [31:0] regs[31:0];
+
+  assign regs[0] = 32'b0;
+
+  assign r_dat0  = regs[r_reg0];
+  assign r_dat1  = regs[r_reg1];
+
+  always @(posedge clk) begin
+    if (write & |w_reg) regs[w_reg] <= w_dat;
+  end
+
+endmodule
