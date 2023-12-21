ceb14708d6bc272ecb8bad154723b10953981ac8
Tung Nguyen <tunghnguyen.ac@proton.me>
2023-12-21 12:22:05 +0700

    added placeholders for memories

---
+++ src/dat_mem.sv
@@ -0,0 +1,12 @@
+module dat_mem (
+    input wire [31:0] addr,
+    input wire [31:0] w_dat,
+    input wire mem_read,
+    input wire mem_write,
+    input wire clk,
+    output reg [31:0] r_dat
+);
+
+
+
+endmodule
---
+++ src/inst_mem.sv
@@ -0,0 +1,9 @@
+module inst_mem (
+    input wire [31:0] addr,
+    input wire clk,
+    output reg [31:0] inst
+);
+
+
+
+endmodule
