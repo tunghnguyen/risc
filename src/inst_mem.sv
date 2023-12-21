476b6378e67188393edf41064b113eb6f1e6f88a
Tung Nguyen <tunghnguyen.ac@proton.me>
2023-12-21 19:58:03 +0700

    added instruction memory

--- src/inst_mem.sv
+++ src/inst_mem.sv
@@ -4,6 +4,14 @@ module inst_mem (
     output reg [31:0] inst
 );
 
+  reg [7:0] mem[2048];
 
+  initial begin
+    $readmemh("../memory/inst.mem", mem);
+  end
+
+  always_ff @(posedge clk) begin
+    inst <= mem[addr+:4];
+  end
 
 endmodule
